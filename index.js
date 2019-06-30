afe8051ccd48f08879a66014cd1f77986f63bc37
David Heller <david@ehellers.com>
2019-06-30 03:10:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var Datastore = require('@google-cloud/datastore').Datastore;
 var datastore;
 
 module.exports = function (options) {
-  options = options || {};
-  var property = options.property || 'db';
-  delete options.property;
+	options = options || {};
+	var property = options.property || 'db';
+	delete options.property;
 
-  if (!datastore) datastore = new Datastore(options || {});
+	if (!datastore) datastore = new Datastore(options || {});
 
-  return function (req, res, next) {
-    req[property] = datastore;
-    next();
-  };
+	return function (req, res, next) {
+		req[property] = datastore;
+		next();
+	};
 };
